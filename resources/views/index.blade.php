4f87a6b3935aed553040ce7a3ed6ad75b823d5b1
Cybernetics <mail@cybernetics.me>
2025-04-28 16:48:05 +0530

    small updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -72,7 +72,7 @@
         <!-- Start:: row-1 -->
         <div class="row">
             <h5 class="text-center">Customer Care Number: +91 9446063521 (Sunday to Friday 8:00 AM - 6:00 PM)</h5>
-            <div class="section-title center-block text-center mb-6">
+            <div class="section-title center-block text-center mb-6 mt-5">
                 <h2>Latest News</h2>
             </div>
             <div class="card bg-primary-transparent01">
