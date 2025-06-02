b4eb3705d2f2c61d6a88a01dee5f8a5563a914e9
Cybernetics <mail@cybernetics.me>
2025-06-02 17:31:19 +0530

    small updates

--- resources/views/category/matrimonial_detail.blade.php
+++ resources/views/category/matrimonial_detail.blade.php
@@ -86,8 +86,8 @@
                             <p>Height: {{ $ad->height }} CM</p>
                             <p>Religion: {{ $ad->religionn?->name }}</p>
                             <p>Caste: {{ $ad->castee?->name }}</p>
-                            <p>Qualification: {{ $ad->typeDetail?->value }}</p>
-                            <p>Occupation: {{ $ad->typeDetail?->value }}</p>
+                            <p>Qualification: {{ $ad->qualificationn?->value }}</p>
+                            <p>Occupation: {{ $ad->occupationn?->value }}</p>
                             <p>Income: {{ $ad->income }} / Year</p>
                         </div>
                     </div>
